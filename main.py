d87e201a9a5ab15b89a2166ff9028ecd6472b533
Jean <egorsvistula1999@gmail.com>
2024-03-09 16:23:41 +0200

    removed bug (response) add 2 def: save_to_html & read_from_html

--- main.py
+++ main.py
@@ -1,50 +1,56 @@
 # pip install requests parsel lxml
-# pip install
 
 import json
 import requests
-from parsel import Selector
+from parsel import Selector, SelectorList
 from requests import Response
 
+def save_to_html(response, filename) -> None:
+    with open(f'{filename}.html', 'w', encoding='utf-8') as file:
+        file.write(response.text)
+
+
+def read_from_html(filename) -> str:
+    with open(f'{filename}.html', 'r') as file:
+        html = file.read()
+
+    return html
 
 
 def parse(response: Response, headers: dict) -> list[dict]:
-    data = []
+    data: list[dict] = []
 
     while True:
-        selector = Selector(text = response.text)
-        quotes = selector.css(".quote")
+        selector: Selector = Selector(text=response.text)
+        quotes: SelectorList = selector.css('.quote')
         for quote in quotes:
-
-            text = quote.css(".text::text").get().strip()[1:-1]
-            author = quote.css("[itemprop='author']::text").get().strip()
-            link = response.url + quote.css("span a::attr(href)").get()[1:]
-
-            tags: list[dict] = [{
-                "name": tag.css("::text").get().strip(),
-                "link": response.url + tag.css("::attr(href)").get()[1:]
-            }
-                for tag in quote.css(".tags .tag")
+            text: str = quote.css('.text::text').get().strip()[1:-1]
+            author: str = quote.css('[itemprop="author"]::text').get().strip()
+            link: str = response.url + quote.css('span a::attr(href)').get()[1:]
+            tags: list[dict] = [
+                {
+                    'name': tag.css('::text').get().strip(),
+                    'link': response.url + tag.css('::attr(href)').get()[1:]
+                }
+                for tag in quote.css('.tags .tag')
             ]
 
             data.append({
-                "text": text,
-                "author": author,
-                "tags": tags,
-                "link": link
+                'text': text,
+                'author': author,
+                'link': link,
+                'tags': tags
             })
 
-        next_button: str = selector.css(".pager .next a::attr(href)").get()
+        next_button: str = selector.css('.pager .next a::attr(href)').get()
         if next_button:
-            url = "https://quotes.toscrape.com" + next_button
-            print(url)
-            response: Response = response.get(url=url, headers=headers)
+            url = 'https://quotes.toscrape.com' + next_button
+            response: Response = requests.get(url=url, headers=headers)
 
         else:
             break
 
-
-        return data
+    return data
 
 
 headers = {
@@ -54,6 +60,7 @@ headers = {
 url = 'https://quotes.toscrape.com'
 
 response: Response = requests.get(url=url, headers=headers)
+save_to_html(response=response, filename='quotes')
 
 quotes_to_scrape_data = parse(response, headers)
 print(json.dumps(quotes_to_scrape_data, indent=2, ensure_ascii=False))
